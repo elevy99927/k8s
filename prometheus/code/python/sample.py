103073251fc683a78e446c1f667b88bf4916ab6f
Eyal Levy <eyallevt@menoramivt.net>
2022-11-26 23:51:59 +0200

    add python example, add PodMonitor example

---
+++ prometheus/code/python/sample.py
@@ -0,0 +1,24 @@
+from prometheus_client import start_http_server, Summary
+from prometheus_client import Counter
+
+import random
+import time
+c = Counter('my_failures', 'Description of counter')
+
+
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        process_request(random.random())
+        c.inc()
---
+++ prometheus/yaml/01-create-animals-deploy.sh
@@ -0,0 +1,1 @@
+kubectl create deployment metricapp --image yanivomc/metrics_app --port 8080 --replicas 3
